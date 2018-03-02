c5292fe9faa1f04ed37b09f7ea9a01ce32b3e7bb
megatron2000 <36814388+megatron2000@users.noreply.github.com>
2018-03-02 22:45:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<head>
+		<title>Lines</title>
+		<link rel="stylesheet" href="base.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	
+	<body>
+		<header class="grid-container">
+			<div class="contact-mail">
+				For your information<br>
+				<a href="mailto:info@lines.com">info@lines.im</a>
+			</div>
+			<select class="choose-lang">
+				<option>us</option>
+				<option>ру</option>
+				<option>ch</option>
+			</select>
+			<div class="mainpage-lines-logo">Lines</div>
+			<div class="mainpage-menu">is <a href="#">super-easy to use</a><br>
+				and <a href="#">completely anonymous</a><br>
+				<span class="strike"><span class="strike-inner">crypto</span></span>currency with <a href="#">~0 fee</a>,<br>
+				<a href="#">hyper-fast transactions</a><br>
+				& <a href="#">big plans</a> for the future
+			</div>
+			<div class="mainpage-download-wallet">
+				<span>To start using just download</span> 
+				<div class="button-block">
+					<button class="blue-button">wallet for Windows</button>
+					<div class="another-os">or for <a href="#">macOS</a>, or for <a href="#">Linux</a></div>
+				</div>
+			</div>
+		</header>
+		<nav>
+			<a href="#" class="no-underline">logo</a>
+			<a href="#about-lines-block" class="active">About Lines</a>
+			<a href="#for-investors-block">For Investors</a>
+			<a href="#for-developers-block">Development</a>
+			<button class="blue-button">Download wallet</button>
+		</nav>
+		<section id="about-lines-block" class="grid-container">
+			<h1><span class="strike"><span class="strike-inner">Crypto</span></span>money<br>for everyone</h1>
+			<p class="subheader">Такие же удобные, как и привычные старые деньги, но</p>
+			<div id="about-secure">
+				<div class="pict"></div>
+				<h4>Completely secure & private</h4>
+				<p>
+					Все операции, поступления и списания со счёта известны только вам. Вы буквально единственный человек с доступом к информации по своему счёту. <br>
+					А ситуации со взломом счетов в принципе невозможны.
+				</p>
+			</div>
+			<div id="about-tech">
+				<div class="pict"></div>
+				<h4>More technological</h4>
+				<p>
+					Лайнс легко передаются на другой телефон с лайнс-волет при помощи NFC. <br>
+					Это быстро и совершенно бесплатно.
+				</p>
+			</div>
+			<aside>
+				<div class="blue-block">
+					<h4>Almost<br>zero fees</h4>
+					<p>
+						Система нетребовательна к ресурсам и способна работать только за счёт комиссий за переводы, размер которых ничтожно мал по сравнению с комиссиями за операции в банках.
+					</p>
+				</div>
+				<div>
+					<h4>+ the fastest <br><span class="strike"><span class="strike-inner">blockchain</span></span></h4>
+					<p>
+						Which handles more than 200 billion transactions per second.
+					</p>
+				</div>
+			</aside>
+		</section>
+
+		<section id="latest-news" class="grid-container">
+			<h1>Latest news</h1>
+			<aside>
+				<div class="soc-nets blue-block">
+					<h4>Check news<br>& updates on<br>our pages</h4>
+					<ul>
+						<li><a href="https://t.me/linescoin">Telegram channel</a></li>
+						<li><a href="https://reddit.com/r/linescoin">Reddit thread</a></li>
+						<li><a href="https://facebook.com/groups/214252599152288">Facebook</a></li>
+						<li><a href="https://twitter.com/linescoin">Twitter</a></li>
+						<li><a href="https://bitcointalk.org/">Bitcointalk.org</a></li>
+						<li><a href="https://github.com/linescoin">Github</a></li>
+					</ul>
+				</div>
+				<div class="email-subscribe">
+					<h4> Or sign up<br>for our weekly<br>newsletter</h4>
+					<p>A letter with the most important once a week</p>
+					<form>
+						<input type="email" placeholder="Your email address" />
+						<input type="button" class="subscribe" disabled />
+					</form>
+				</div>
+			</aside>
+
+			<div class="last-twits-block">
+				<div class="twit">
+					<div class="twit-date">3 hours ago</div>
+					<a class="retwit-link" href="#"><span>retweet</span></a>
+					<p>
+						We are pleased to announce that Emurgo have partnered  with SIRIN Labs to integrate <a class="hashtag-link" href="#">#Lines</a> with the FINNEY blockchain smartphone! Read more here:
+					</p>
+					<a href="#" class="attached-link no-underline">
+						<img></img>
+						<div class="link-descript">
+							<h7>Sirin Lab’s Blockchain Smartphone Pre-Integration...</h7>
+							<p>
+								The Project ICO raised $157.8 million in the fourth largest token sale in history.
+							</p>
+							<div class="link-source">financemagnates.com</div>
+						</div>
+					</a>
+				</div>
+				<div class="twit">
+					<div class="twit-date">7 hours ago</div>
+					<a class="retwit-link" href="#"><span>retweet</span></a>
+					<p>
+						Our developers are testing “<a class="hashtag-link" href="#">#Lines</a> — wallet” v0.0.0.1! <br>
+						This will be the first <a class="hashtag-link" href="#">#blockchain</a> wallet to allow sending <a class="hashtag-link" href="#">#SEGWIT</a> transactions from the GUI interface.<br>
+						All Lines SEGWIT addresses will start with a "lns" prefix:
+					</p>
+					<div class="attached-gallery">
+						<a href="#" class="attached-pic no-underline"><img src="" /></a>
+						<a href="#" class="attached-pic no-underline"><img src="" /></a>
+						<a href="#" class="attached-pic no-underline"><img src="" /></a>
+					</div>
+				</div>
+				<div class="twit">
+					<div class="twit-date">1 day ago</div>
+					<a class="retwit-link" href="#"><span>retweet</span></a>
+					<p>
+						New <a class="hashtag-link" href="#">#Lines</a> Build 2.1.6<br><br>
+						Windows installer/Binaries at Sourceforge:<br>
+						<a href="#">https://sourceforge.net/projects/linescoin/files …</a><br><br>
+						Source code and binaries at Github:<br>
+						<a href="#">https://github.com/LinesCoin/LinesCoin/releases/tag/2.1.6 ... </a><br><br>
+						<a class="hashtag-link" href="#">#Lines</a> <a class="hashtag-link" href="#">#Altcoin</a> <a class="hashtag-link" href="#">#Bitcoin</a>
+					</p>
+					<a href="#" class="attached-pic no-underline"><img src="" /></a>
+				</div>
+				<div class="block-follow-us"><button class="blue-button">follow @linescoin</button></div>
+			</div>
+		</section>
+		<section id="market-cap-block">
+			<div class="rate-graph"></div>
+			<div class="mc-numbers">
+				<div class="mc-header"><b>Market Capitalisation</b><span class="mc-btc">BTC 277 047</span><span class="mc-eth">ETH 3 296 023</span></div>
+				<div class="mc-usd">$2 846 847 010</div>
+			</div>
+			<button><div class="icon"></div><label>test<br>button</label></button>
+		</section>
+		<section id="for-investors-block" class="grid-container">
+			<h1>For <span class="strike"><span class="strike-inner">Crypto</span></span>Ivestors</h1>
+			<div class="whitepaper-block">
+				<p class="subheader">Lines super, plans are great, <br>watch roadmap, whitepaper read yo</p>
+				<button class="large-white-button whitepaper-button">view whitepaper</button>
+			</div>
+			<div class="roadmap">
+				<div class="plans-for-2k18 grid-container">
+					<div class="year-header">
+						Plans for<span>2018</span>
+					</div>
+					<div class="quarters">
+						<div id="Q1-18" class="quarter">
+							<div class="quarter-header">Q1</div>
+							<div class="months-block">
+								<div class="month done">
+									<b class="month-name">January</b>
+									<p>Start project development<i>v</i></p>
+								</div>
+								<div class="month done">
+									<b class="month-name">February</b>
+									<p>Release <b>Lines Network</b><i>β</i> and test it by dev-team<i>v</i></p>
+								</div>
+								<div class="month">
+									<b class="month-name">March</b>
+									<p>Release <b>Lines Network 1.0</b> and «<b>Lines wallet</b>» for Windows, macOS  and Linux</p>
+								</div>
+							</div>
+						</div>
+						<div id="Q2-18" class="quarter">
+							<div class="quarter-header">Q2</div>
+							<div class="months-block">
+								<div class="month">
+									<b class="month-name">April</b>
+									<p>Listings on <a href="https://tradeogre.com/"><b>TradeOgre</b></a></p>
+								</div>
+								<div class="month">
+									<b class="month-name">May</b>
+									<p>Release «<b>Lines wallet</b>» for Android and listings on <a href="https://coinhouse.eu"><b>CoinHouse</b></a></p>
+								</div>
+								<div class="month">
+									<b class="month-name">June</b>
+									<p>Release Lines Network 1.0 and «Lines — wallet» for Windows, macOS  and Linux</p>
+								</div>
+							</div>
+						</div>
+						<div id="Q3-18" class="quarter">
+							<div class="quarter-header">Q3</div>
+							<div class="months-block">
+								<div class="month">
+									<b class="month-name">July</b>
+									<p>Project development start<i>v</i></p>
+								</div>
+								<div class="month">
+									<b class="month-name">August</b>
+									<p>Release Lines Network <span>β</span> and test it by dev-team<i>v</i></p>
+								</div>
+								<div class="month">
+									<b class="month-name">September</b>
+									<p>Release Lines Network 1.0 and «Lines — wallet» for Windows, macOS  and Linux</p>
+								</div>
+							</div>
+						</div>
+						<div id="Q4-18" class="quarter">
+							<div class="quarter-header">Q4</div>
+							<div class="months-block">
+								<div class="month">
+									<b class="month-name">October</b>
+									<p>Project development start<i>v</i></p>
+								</div>
+								<div class="month">
+									<b class="month-name">November</b>
+									<p>Release Lines Network <span>β</span> and test it by dev-team<i>v</i></p>
+								</div>
+								<div class="month">
+									<b class="month-name">December</b>
+									<p>Release Lines Network 1.0 and «Lines — wallet» for Windows, macOS  and Linux</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="plans-for-2k19 grid-container">
+					<div class="year-header">
+						Plans for<span>2019</span>
+					</div>
+					<div class="quarters">
+						<div id="Q1-19" class="quarter">
+							<div class="quarter-header">Q1</div>
+							<div class="months-block">
+								<div class="month">
+									<b class="month-name">January</b>
+									<p>Project development start<i>v</i></p>
+								</div>
+								<div class="month">
+									<b class="month-name">February</b>
+									<p>Release Lines Network <span>β</span> and test it by dev-team<i>v</i></p>
+								</div>
+								<div class="month">
+									<b class="month-name">March</b>
+									<p>Release Lines Network 1.0 and «Lines — wallet» for Windows, macOS  and Linux</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<aside class="blue-block">
+				<h4>4,000,000 LNS</h4>
+				is circulating supply
+
+				<h4>from 8,000,000,000</h4>
+				of total supply
+
+				<h4>& no premine</h4>
+			</aside>
+		</section>
+
+		<section id="exchanges-block" class="grid-container">
+			<div class="listed-on">
+				<h5>LNS have already listed on</h5>
+				<a href="#">kucoin</a>
+				<a href="#">exmo</a>
+				<a href="#">huobi.pro</a>
+				<a href="#">binance</a>
+			</div>
+			<div class="expect-the-listings">
+				<h5>And expect the listings</h5>
+				<a href="#">forkdelta</a>
+				<a href="#">gdax</a>
+			</div>
+		</section>
+
+		<section id="for-developers-block" class="grid-container">
+			<h1>/for_developers</h1>
+			<p class="subheader">
+				lines has a fully open source code based on the <a href="#"><span>cryptonote</span></a> protocol
+			</p>
+			<p>
+				all our developments (including all desktop and mobile wallets, miner, block explorer, etc.) are available in <a href="#"><b>our github repository</b></a>
+			</p>
+
+			<p>
+				if you find some bugs in lines applications, please email us at <a href="#"><b>bugs@lines.im</b></a><br>
+				or post it in <a href="#"><b>our chat in discord</b></a>
+			</p>
+
+			<aside class="blue-block">
+				<h4>else {</h4>
+				<p>
+					if you want to join our dev–team just email us at <a href="#"><b>job@lines.im</b></a>
+				</p>
+			</aside>
+
+			<footer class="grid-container">
+				<div class="copyright">
+					© 2018
+				</div>
+
+				<div class="footer-links">
+					<a href="https://terms.lines.im">Terms of use</a>
+					<a href="https://brand.lines.im">Brand</a>
+				</div>
+
+				<div class="soc-nets">
+					<a class="no-underline" href="https://t.me/linescoin">tg</a>
+					<a class="no-underline" href="https://reddit.com/r/linescoin">rd</a>
+					<a class="no-underline" href="https://facebook.com/groups/214252599152288">fb</a>
+					<a class="no-underline" href="https://twitter.com/linescoin">tw</a>
+					<a class="no-underline" href="https://bitcointalk.org/">bt</a>
+					<a class="no-underline" href="oyaebu1in35.onion">oyaebu1in35.onion</a>
+				</div>
+			</footer>
+		</section>
+	</body>
+	
+</html>
